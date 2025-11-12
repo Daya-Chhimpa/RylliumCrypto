f6852dd70ec8e0475de0210833a5526ad58953ed
Daya-Chhimpa <dayakiran97285@gmail.com>
2025-11-12 14:58:51 +0530

    ai url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://backend-dev.pprince.io";
+// const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://backend-dev.pprince.io";
+const BASE_URL = "https://backend-dev.ryllium.com";
 
 const AUTH_TOKEN_KEY = "authToken";
 
