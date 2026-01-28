d708ad6bbb72b0b0c1433f1eaf0b14a3ce6b5c71
dayachhimpa <dayakiran97285@gmail.com>
2026-01-28 22:21:37 +0530

    invalid isssue fixed

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,7 @@
-
-const BASE_URL = "api.alpacross.com";
+// const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://backend-dev.pprince.io";
+// const BASE_URL = "https://backend-dev.alpacross.com";
+// const BASE_URL = "api.alpacross.com";
+const BASE_URL = "https://api.alpacross.com";
 const AUTH_TOKEN_KEY = "authToken";
 
 export function setAuthToken(token) {
